f4893914fa0cad2a15fedb56d45428c71dd232a7
Alexander Montgomery <evaderxander@gmail.com>
2017-11-30 16:25:33 -0500

    Updated Readme and added ten second sleep so user can see the file

--- README.md
+++ README.md
@@ -1,2 +1,73 @@
 # adobe-cs6-patcher
 A private patcher for adobe cs6
+
+## Getting Started
+
+This a rust based patch that is meant only for the patching of adobe CS6 products. This project includes and compiles the two patch files needed. As this is a rust project, you will need cargo and this patch only runs on the windows version of this application. 
+
+### Prerequisites
+
+What things you need to install the software and how to install them
+
+To build this patcher from source code run the following command. 
+
+```
+cargo build --release
+```
+
+Each of these arguments does the following.
+
+cargo: The rust package manager.
+build: build the binary for this software. 
+--release: A binary optimized version
+
+
+### Installing
+
+This patcher does not actually keep anything on the system long term other than the patched file. 
+
+However make sure that you either change the glob to search the right drive or use the standard C drive.
+
+## Running the tests
+
+This application has very little code. Roughly 70 lines. 
+So there is a single unit test, however that unit test is the standard 2+2=4.
+
+cargo test 
+
+### Coding style
+
+This project follows all rust standards and should compile without any warnings. 
+If not, bad Xander~! Go fix it. 
+
+## Deployment
+
+When running the compiled binary, make sure to run it as Administrator mode in windows. 
+
+If you forget, nothing terrible will happen, it will just fail when trying to remove the old file. 
+
+Under those circumstances, a reinstall will not be needed. Just run the patcher again.
+
+## Built With
+
+* [Rust 1.22.1 (05e2e1c41 2017-11-22)](http://www.dropwizard.io/1.0.2/docs/) - Language Used
+* [Glob 0.2.11](https://github.com/rust-lang-nursery/glob) - Used for finding all the files that need patched
+* [blake 0.7](https://rometools.github.io/rome/) - Used for the hashing technology
+* [digest "0.7"]() - Used for converting file object to hash 
+## Contributing
+
+Please read [CONTRIBUTING.md](https://gist.github.com/PurpleBooth/b24679402957c63ec426) for details on our code of conduct, and the process for submitting pull requests to us.
+
+## Versioning
+
+We use [SemVer](http://semver.org/) for versioning. For the versions available, see the [tags on this repository](https://github.com/your/project/tags). 
+
+## Authors
+
+* **Alexander Montgomery** - *Initial work* - [recursiverighthook](https://github.com/recursiverighthook/adobe-cs6-patcher)
+
+See also the list of [contributors](https://github.com/recursiverighthook/adobe-cs6-patcher/graphs/contributors) who participated in this project.
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details
--- src/main.rs
+++ src/main.rs
@@ -2,42 +2,46 @@ extern crate glob;
 extern crate blake2;
 
 use glob::glob;
-use std::io::Write;
-use blake2::{Blake2b, Digest};
 use std::fs::File;
+use std::io::Write;
 use std::path::PathBuf;
+use std::{thread, time};
 use std::result::Result;
+use blake2::{Blake2b, Digest};
+
 
 
 fn main() {
     println!("Finding amtlib.dll files");
     let files = find_core_files();
-    println!("Found {:?} files to patch", files.len());
+    println!("Found {:?} posible files to patch", files.len());
     for x in files {
         let hash = hash_files(&x);
         replace_old_files(&hash, &x);
     }
+    println!("All Files Patched Successfully.");
+    thread::sleep(time::Duration::from_millis(10000));
 }
 
 //
-// The stored values in this method has the md5 hashes from all the established core files that need replaced.
+// The stored values in this method has the md5 hashes from all the established core files that need replaced. 
+// Hashing is preformed using Blake2b (Blake2 was introduced to replace md5 and SHA-1 after both were considered to be broken)
 //
 fn replace_old_files(hash: &String, path: &PathBuf) {
 
     let x86_bytes = include_bytes!("32-bit/amtlib.dll");
     let x64_bytes = include_bytes!("64-bit/amtlib.dll");
 
-    let hash = hash.as_str();
-
     let x86_files = vec![
         "12edb238aaaa50168b58e5684f4ffec388575d4a4cf007c46edc0d304c055ab94e40225b920d9c791669da7bd67a147a5117de0dff8b365c236457c427a81bb5",
         "44cf6af23abe1eabcde29a8c53baef4361e3fc7492bc249019ea8426358d4741994d7789f625b3503b7f57ab628318befffc5a11d6442f75be1a3e80a46337e9",
         "54f69efd224dd338309a1a6aa09116b7ea3f907353020a832546c50d482e5e6e176cf1cfd06f13486db732e82b9c8274a2387b03d2bb3d4beac9cb2d00e93e5a"];
+
     let x64_files = vec![
         "017e4d759e36931f79c7eb61aafad48ef3b24cf852d1552ddf715ecf4459e86ec0c6aee21a995084976b3cd9223994cbe4a5379de2864b4e8b15ecdaa59a0a0b",
         "b2f33bde6d5a3f284e81b5c1819627bd04cc81d5ccec32f3c10ae34e4ec058ed3ddb5beb90fcd3469d4614609c9fb2c275d4babd6b7b7a5e1b4ea1bd7a5e0f8c"];
 
-    if x86_files.contains(&hash) {
+    if x86_files.contains(&hash.as_str()) {
         let p = path.as_path();
         std::fs::remove_file(&p).expect("Unable to remove a file");
         let mut cracked_file = File::create(&p).expect("Unable to create cracked file, a reinstall will now be required.");
@@ -45,7 +49,7 @@ fn replace_old_files(hash: &String, path: &PathBuf) {
         println!("x86 file used {:?}", &p);
     }
 
-    else if x64_files.contains(&hash) {
+    else if x64_files.contains(&hash.as_str()) {
         let p = path.as_path();
         std::fs::remove_file(&p).expect("Unable to remove a file");
         let mut cracked_file = File::create(&p).expect("Unable to create cracked file, a reinstall will now be required.");
@@ -54,17 +58,22 @@ fn replace_old_files(hash: &String, path: &PathBuf) {
     }
 }
 
+
+//
+// Given a file use the Blake algorithim and crypto acceptable algorithim. 
+//
 fn hash_files(f: &PathBuf) -> String {
     let path = f.as_path();
     let mut file = File::open(&path).expect("Unable to open");;
     let hash = Blake2b::digest_reader(&mut file).expect("Unable to read file");
-
     let hash_code = format!("{:x}", hash);
-
     return hash_code;
 }
 
 
+//
+// Scan the entire C: drive and looks for all amtlib.dll files. The actual file that needs patched.
+//
 fn find_core_files() -> Vec<PathBuf> {
     let paths: Vec<_> =  glob("C:/**/amtlib.dll").unwrap().filter_map(Result::ok).collect();
     return paths;
